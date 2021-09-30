a484e0598b1d2fc299814101ba892ce071abaff1
henriquenobre <henriquenobr@gmail.com>
2021-09-30 16:42:34 -0300

    oi

---
+++ alfabeto.js
@@ -0,0 +1,16 @@
+//mostra qual letra do alfabeto esta faltando
+function fearNotLetter(str) {
+    for (var i = 0; i < str.length; i++) {
+      /* code of current character */
+      var code = str.charCodeAt(i);
+  
+      /* if code of current character is not equal to first character + no of iteration
+          hence character has been escaped */
+      if (code !== str.charCodeAt(0) + i) {
+        /* if current character has escaped one character find previous char and return */
+        return String.fromCharCode(code - 1);
+      }
+    }
+    return undefined;
+  }
+  console.log(fearNotLetter("abce"));
